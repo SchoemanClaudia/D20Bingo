68a675a8a2ec7f21c73e0d680e25688f725678d1
Claudia Schoeman <claudiaschoeman1@gmail.com>
2024-07-20 23:09:07 +0000

    Randomize D20 dice img roll on button click in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,19 @@ var sound = document.querySelector(".btn-roll");
 // Sound added to dice roll - https://pixabay.com/sound-effects/rpg-dice-rolling-95182/
 sound.addEventListener("click", function(){
     new Audio("assets/sounds/diceSound.mp3").play();
+
+    diceRoll();
 });
 
+/**
+ * Rolls through D20 dice img at random from 1 to 20
+ * https://www.youtube.com/watch?v=UkmNL7eJqsU
+ */
+function diceRoll(){
+    var random = Math.ceil(Math.random()*20 + 1);
+    var imagePath = "assets/images/dice" + random + ".webp";
+
+    document.querySelectorAll(".dice-img").forEach(function(dice){
+        dice.setAttribute("src", imagePath);
+    })
+};
